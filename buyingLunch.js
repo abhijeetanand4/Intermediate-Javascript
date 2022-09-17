14ef800fd8b17f4d530d1a9529553b124da72a4e
abhijeetanand4 <105948259+abhijeetanand4@users.noreply.github.com>
2022-09-17 20:10:46 +0530

    Create buyingLunch.js

---
+++ buyingLunch.js
@@ -0,0 +1,14 @@
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+    
+    //Write your code here.
+    var number = Math.random()*(names.length);
+    number = Math.floor(number);
+    return (names[number] + " is going to buy lunch today!");
+    
+    
+
+
+/******Don't change the code below*******/    
+}
