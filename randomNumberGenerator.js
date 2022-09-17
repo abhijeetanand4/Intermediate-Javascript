45c4419a95c4f31e9b21bd4ab029db4727148a59
abhijeetanand4 <105948259+abhijeetanand4@users.noreply.github.com>
2022-09-17 18:49:05 +0530

    Create randomNumberGenerator.js

---
+++ randomNumberGenerator.js
@@ -0,0 +1,12 @@
+//random number generation
+prompt("Enter your name");
+prompt("Enter lover name");
+var n = Math.random(n);
+n = n * 100;
+var calc = Math.floor(n + 1);
+if(calc>50){
+    alert(calc + " strong");
+}
+else{
+    alert(calc + " not strong");
+}
